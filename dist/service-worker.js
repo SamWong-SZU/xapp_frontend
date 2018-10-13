3691912756f43d9be8b107c4d99458f6af9d6815
SamWong <200988743@qq.com>
2018-10-13 14:19:05 +0800

    fix build

---
+++ dist/service-worker.js
@@ -0,0 +1,53 @@
+importScripts("/xapp_frontend/dist/precache-manifest.e5b45f76b9f4543fbe06155181c4078b.js");
+
+
+        importScripts('/xapp_frontend/dist/static/workbox-v3.6.2/workbox-sw.js');
+        workbox.setConfig({
+            modulePathPrefix: '/xapp_frontend/dist/static/workbox-v3.6.2/'
+        });
+    /**
+ * @file service-worker.js with workbox api
+ * @desc [example](https://workbox-samples.glitch.me/examples/workbox-sw/)
+ * @author SamWong(200988743@qq.com)
+ */
+
+/* globals workbox */
+workbox.core.setCacheNameDetails({
+    prefix: 'lavas-cache',
+    suffix: 'v1',
+    precache: 'install-time',
+    runtime: 'run-time',
+    googleAnalytics: 'ga'
+});
+workbox.skipWaiting();
+workbox.clientsClaim();
+
+workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
+
+/**
+ * example runningCache with api
+ */
+// workbox.routing.registerRoute(/^https:\/\/lavas\.baidu\.com\/some\/api/,
+//     workbox.strategies.networkFirst());
+
+
+/**
+ * example runningCache with resources from CDN
+ * including maxAge, maxEntries
+ * cacheableResponse is important for CDN
+ */
+// workbox.routing.registerRoute(/^https:\/\/cdn\.baidu\.com/i,
+//     workbox.strategies.cacheFirst({
+//         cacheName: 'lavas-cache-images',
+//         plugins: [
+//             new workbox.expiration.Plugin({
+//                 maxEntries: 100,
+//                 maxAgeSeconds: 7 * 24 * 60 * 60
+//             }),
+//             new workbox.cacheableResponse.Plugin({
+//                 statuses: [0, 200]
+//             })
+//         ]
+//     })
+// );
+workbox.routing.registerNavigationRoute('/xapp_frontend/dist/index.html');
